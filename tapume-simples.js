d027be967dad0c114836d03d1d071d342d68343d
Elias Ferreira <hazardts@gmail.com>
2021-09-04 09:12:30 -0300

    adicionando vtexcommercestable

--- tapume-simples.js
+++ tapume-simples.js
@@ -1,14 +1,11 @@
 function ativaTapume(){
 	let url = window.location.hostname.split('.');
-	console.log(url[1]);
 	let newUrl = "https://"+window.location.hostname+"/manutencao";
-	if(url[1] == "myvtex"){
+	if(url[1] == "myvtex" || url[1] == "vtexcommercestable"){
 		document.querySelector('body').classList.remove('hide-tapume')
 	} else {
 		if(window.location.href != newUrl){
-			
 			window.location.href = newUrl;
-			console.log(window.location.href,newUrl);
 		}
 	}
 }
